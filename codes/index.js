b7754bac9bacbdddab120de28b8fb61fddc7e122
Samed Shukurov <samedzauroglu2007@gmail.com>
2023-05-25 11:36:15 +0400

    added new jokes

--- codes/index.js
+++ codes/index.js
@@ -4,3 +4,15 @@ alert(
 
 console.log("Nə  gəzirsən burda çox bilmiş");
 console.log("Get burdan");
+let continued = document.querySelector(".continued");
+
+continued.addEventListener("click",()=>{
+  continued.classList.add("active")
+  //   continued.classList.toggle("active")
+  if(continued.className === "active"){
+    continued.classList.remove("continued")
+  }
+  else{
+    continued.classList.add("continued")
+  }
+})
